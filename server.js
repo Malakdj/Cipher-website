ecc8a9d98743aaf488a07b59f60685fdf2568996
Malakdj <malak31m204@email.com>
2025-04-12 22:46:14 +0200

    Updated server configuration for new folder structure

--- server.js
+++ server.js
@@ -1,57 +1,27 @@
-const express = require("express");
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const { exec } = require('child_process');
+const fs = require('fs');
 
 const app = express();
-app.use(express.json());
-// app.use(express.static(path.join(__dirname, "pages"))); // Serve HTML pages from 'pages' folder
-app.use(express.static(path.join(__dirname))); // Serve from root
-const codesDir = path.join(__dirname, "codes"); // Path to C++ files
 
-app.post("/run", (req, res) => {
+// Serve static files from correct locations
+app.use(express.static(path.join(__dirname, 'pages'))); // For HTML files
+app.use('/css', express.static(path.join(__dirname, 'pages/css'))); // For CSS
+
+// API endpoints for encryption/decryption
+app.post('/run', (req, res) => {
     const { algorithm, key, message } = req.body;
-    console.log("Received algorithm:", algorithm);
-
-    const cppFile = path.join(codesDir, `${algorithm}.cpp`);
-    if (!fs.existsSync(cppFile)) {
-        return res.status(400).send(`Error: ${algorithm}.cpp not found.`);
-    }
-
-    fs.writeFileSync("input.txt", key + "\n" + message);
-    const command = `g++ \"${cppFile}\" -o \"${codesDir}/${algorithm}.exe\" && \"${codesDir}/${algorithm}.exe\"`;
+    const cppFile = path.join(__dirname, 'codes', `${algorithm}.cpp`);
     
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            res.send("Compilation or Execution Error: " + stderr);
-        } else {
+    fs.writeFileSync('input.txt', `${key}\n${message}`);
+    exec(`g++ "${cppFile}" -o "${algorithm}.out" && ./${algorithm}.out`, 
+        (error, stdout, stderr) => {
+            if (error) return res.status(500).send(stderr);
             res.send(stdout);
         }
-    });
-    
+    );
 });
 
-app.post("/runDecryption", (req, res) => {
-    const { algorithm, key, message } = req.body;
-    console.log("Received decryption request for algorithm:", algorithm);
-
-    const cppFile = path.join(codesDir, `${algorithm}_decrypt.cpp`);
-    if (!fs.existsSync(cppFile)) {
-        return res.status(400).send(`Error: ${algorithm}_decrypt.cpp not found.`);
-    }
-
-    fs.writeFileSync("input.txt", key + "\n" + message);
-    const command = `g++ \"${cppFile}\" -o \"${codesDir}/${algorithm}_decrypt.exe\" && \"${codesDir}/${algorithm}_decrypt.exe\"`;
-    
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            res.send("Compilation or Execution Error: " + stderr);
-        } else {
-            res.send(stdout);
-        }
-    });
-});
-
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
